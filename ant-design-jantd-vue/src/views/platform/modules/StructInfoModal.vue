a94b1b2f50d54f7d5749ca1c6568590000a2a8df
xueyc@mksoftapp.com <xueyc@mksoftapp.com>
2019-08-13 14:42:14 +0800

    #16428: 结构体管理

---
+++ ant-design-jantd-vue/src/views/platform/modules/StructInfoModal.vue
@@ -0,0 +1,181 @@
+<template>
+  <a-drawer
+    :title="title"
+    :maskClosable="true"
+    :width="800"
+    placement="right"
+    :closable="true"
+    @close="handleCancel"
+    :visible="structModalVisible"
+    style="height: calc(100% - 55px);overflow: auto;padding-bottom: 53px;">
+
+    <a-spin :spinning="confirmLoading">
+      <a-form :form="form">
+        <a-form-item label="结构体名称" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input placeholder="请输入结构体名称" :disabled="disableSubmit" v-decorator="[ 'structName', validatorRules.structName]"/>
+        </a-form-item>
+        <a-form-item label="结构体备注" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input placeholder="请输入结构体备注" :disabled="disableSubmit" v-decorator="[ 'structRemark', validatorRules.structRemark]"/>
+        </a-form-item>
+        <!--<a-form-item label="结构体" :labelCol="labelCol" :wrapperCol="wrapperCol">-->
+          <a-row :gutter="15">
+            <a-col :md="5" :sm="4" offset="2">
+              <span>结构体变量名</span>
+            </a-col>
+            <a-col :md="5" :sm="4">
+              <span>参数类型</span>
+            </a-col>
+            <a-col :md="5" :sm="4">
+              <span>参数单位</span>
+            </a-col>
+            <a-col :md="5" :sm="4">
+              <span>参数含义</span>
+            </a-col>
+          </a-row>
+        <!--</a-form-item>-->
+        <a-form-item v-for="(item, index) in structModalListData" :key="index">
+          <a-row :gutter="15">
+            <a-col :md="5" :sm="4" offset="2">
+              <a-input v-model="item.structVariable" :disabled="disableSubmit"/>
+            </a-col>
+            <a-col :md="5" :sm="4">
+              <a-input v-model="item.structParaType" :disabled="disableSubmit"/>
+            </a-col>
+            <a-col :md="5" :sm="4">
+              <a-input v-model="item.structParaUnit" :disabled="disableSubmit"/>
+            </a-col>
+            <a-col :md="5" :sm="4">
+              <a-input v-model="item.structParaMeaning" :disabled="disableSubmit"/>
+            </a-col>
+            <a-icon type="plus-circle" v-if="!disableSubmit"  class="structAddIcon" @click="addStructInfo(index)"/>
+            <a-icon type="delete" v-if="!disableSubmit" class="structDeleteIcon" @click="deleteStructInfo(index)"/>
+          </a-row>
+        </a-form-item>
+
+      </a-form>
+    </a-spin>
+
+    <div class="drawer-bootom-button" v-show="!disableSubmit">
+      <a-popconfirm title="确定放弃编辑？" @confirm="handleCancel" okText="确定" cancelText="取消">
+        <a-button style="margin-right: .8rem">取消</a-button>
+      </a-popconfirm>
+      <a-button @click="handleSubmit" type="primary" :loading="confirmLoading">提交</a-button>
+    </div>
+  </a-drawer>
+</template>
+
+<script>
+
+  import AFormItem from "ant-design-vue/es/form/FormItem";
+  import ARow from "ant-design-vue/es/grid/Row";
+  import ACol from "ant-design-vue/es/grid/Col";
+  import AInput from "ant-design-vue/es/input/Input";
+
+  export default {
+    name: "StructInfoModal",
+    components: {
+      AInput,
+      ACol,
+      ARow,
+      AFormItem
+
+    },
+    data () {
+      return{
+        title: '',
+        structModalVisible: false,
+        confirmLoading: false,
+        disableSubmit: false,
+        form: this.$form.createForm(this),
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        validatorRules:{
+          structName:{
+            rules: [
+              { required: true, message: '请输入结构体名称!' },
+            ]},
+          structRemark:{
+            rules: [
+              { required: true, message: '请输入结构体备注!'},
+            ]}
+        },
+        structModalListData: [
+          {
+            structVariable: 'name',
+            structParaType: 'string',
+            structParaUnit: '',
+            structParaMeaning: ''
+          }
+        ]
+      }
+    },
+    created () {
+
+    },
+    computed:{
+
+    },
+    methods: {
+      handleCancel () {
+        this.structModalVisible = false;
+      },
+      handleSubmit () {
+
+      },
+      addStructInfo (index) {
+        console.log('addStructInfo index:', index)
+        const self = this;
+        self.structModalListData.splice(index+1,0,{});
+      },
+      deleteStructInfo (index) {
+        const self = this;
+        if (self.structModalListData.length === 1) {
+          this.$notification.open({
+            message: '提示',
+            description: '最后一行无法删除！',
+            duration: 2,
+            style: {
+              width: '600px',
+              marginLeft: `${335 - 600}px`
+            },
+          });
+          return false;
+        }
+        self.structModalListData.splice(index,1);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .structDeleteIcon{
+    font-size: 18px;
+  }
+  .structDeleteIcon:hover{
+    cursor:pointer;
+  }
+  .structAddIcon{
+    font-size: 18px;
+    margin-right: 10px;
+  }
+  .structAddIcon:hover{
+    cursor:pointer;
+  }
+  .drawer-bootom-button {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    border-top: 1px solid #e8e8e8;
+    padding: 10px 16px;
+    text-align: right;
+    left: 0;
+    background: #fff;
+    border-radius: 0 0 2px 2px;
+  }
+</style>
---
+++ ant-design-jantd-vue/src/views/platform/structList.vue
@@ -0,0 +1,256 @@
+<template>
+  <a-row :gutter="10">
+    <a-col :md="8" :sm="24">
+      <a-card :bordered="false">
+        <div style="background: #fff;padding-left:16px;height: 100%; margin-top: 5px">
+          <div>
+            <span class="struct-classification-list-title">结构体分类</span>
+            <a-icon class="struct-classification-list-add-icon" type="plus" @click="addStructClassification" />
+          </div>
+          <a-input-search @search="onSearch" style="width:100%;margin-top: 10px" placeholder="搜索"/>
+          <div v-for="(item, index) in structListData" class="struct-classification-list-item" :key="index">
+            <a-icon class="struct-classification-list-item-icon" type="file-text" />
+            <span class="struct-classification-list-item-name">{{item.value}}</span>
+            <a-icon class="struct-classification-list-item-delete" type="delete" @click="deleteStructClassification" />
+            <a-icon class="struct-classification-list-item-edit" type="edit" @click="editStructClassification" />
+          </div>
+        </div>
+      </a-card>
+    </a-col>
+    <a-col :md="16" :sm="24">
+      <a-card :bordered="false">
+        <div style="padding-bottom: 20px">
+          <a-row :gutter="10">
+            <a-col :md="8" :sm="8">
+              <a-input placeholder="结构体名称" />
+            </a-col>
+            <a-col :md="8" :sm="8">
+              <a-input placeholder="结构体备注" />
+            </a-col>
+            <a-col :md="2" :sm="2" :offset="3">
+              <a-button>搜索</a-button>
+            </a-col>
+            <a-col :md="2" :sm="2">
+              <a-button type="primary" @click="addStructInfo">新增</a-button>
+            </a-col>
+          </a-row>
+        </div>
+        <a-table
+          :columns="structTableColumn"
+          :dataSource="structTableListData"
+          :loading="structTableLoading"
+          :pagination="structTablePagination"
+          rowKey="structNum"
+        >
+          <span slot="structOperation">
+            <a @click="structTableCheck">查看</a>
+            <a-divider type="vertical"/>
+            <a @click="structTableEdit">编辑</a>
+            <a-divider type="vertical"/>
+            <a-popconfirm title="确定删除吗?" @confirm="() => {}">
+              <a>删除</a>
+            </a-popconfirm>
+          </span>
+        </a-table>
+        <StructInfoModal ref="structInfoModalForm" />
+      </a-card>
+    </a-col>
+    <a-modal
+      :title="classificationModalTitle"
+      :visible="classificationModalVisible"
+      @ok="classificationModalOk"
+      :confirmLoading="classificationModalLoading"
+      @cancel="classificationModalCancel"
+    >
+      <a-form
+        :form="form"
+      >
+        <a-form-item
+          label="类名"
+          :label-col="{ span: 5 }"
+          :wrapper-col="{ span: 15 }"
+        >
+          <a-input
+            v-decorator="['structClassificationName',{rules: [{ required: true, message: '请输入结构体分类名称!' }]}]"
+          />
+        </a-form-item>
+      </a-form>
+    </a-modal>
+  </a-row>
+</template>
+<script>
+//  import {JantdListMixin} from '@/mixins/JantdListMixin'
+  import AFormItem from 'ant-design-vue/es/form/FormItem';
+  import ARow from 'ant-design-vue/es/grid/Row';
+  import StructInfoModal from './modules/StructInfoModal';
+
+export default {
+    name: 'DepartUserList',
+//    mixins: [JantdListMixin],
+    components: {
+      ARow,
+      AFormItem,
+      StructInfoModal
+    },
+    data() {
+      return {
+        form: this.$form.createForm(this),
+        structListData: [
+          { key: '0', value: 'test1' },
+          { key: '1', value: 'test12' },
+          { key: '2', value: 'test123' }
+        ],
+        classificationModalTitle: '新增分类',
+        classificationModalVisible: false,
+        classificationModalLoading: false,
+        structTableColumn: [
+          {
+            title: '序号',
+            sorter:  (a, b) => a.structNum - b.structNum,
+            align: 'center',
+            dataIndex: 'structNum'
+          },
+          {
+            title: '结构体名称',
+            sorter: (a, b) => a.structName.length - b.structName.length,
+            align: 'center',
+            dataIndex: 'structName'
+          },
+          {
+            title: '结构体备注',
+            sorter: (a, b) => a.structRemark.length - b.structRemark.length,
+            align: 'center',
+            dataIndex: 'structRemark'
+          },
+          {
+            title: '结构体操作',
+            align: 'center',
+            dataIndex: 'structOperation',
+            scopedSlots: { customRender: 'structOperation' }
+          }
+        ],
+        structTableListData: [
+          {
+            structNum: '1',
+            structName: 'test',
+            structRemark: 'no remark'
+          },
+          {
+            structNum: '2',
+            structName: 'test pro',
+            structRemark: 'no remark pro'
+          }
+        ],
+        structTableLoading: false,
+        structTablePagination: {
+          current: 1,
+          pageSize: 10,
+          pageSizeOptions: ['10', '20', '30'],
+          showTotal: (total, range) => {
+            return range[0] + '-' + range[1] + ' 共' + total + '条'
+          },
+          showQuickJumper: true,
+          showSizeChanger: true,
+          total: 0
+        }
+      }
+    },
+    methods: {
+      onSearch () {
+
+      },
+      classificationModalOk () {
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+            this.classificationModalVisible = false;
+          }
+        });
+      },
+      classificationModalCancel () {
+        this.classificationModalVisible = false;
+      },
+      addStructClassification () {
+        this.classificationModalVisible = true;
+        this.classificationModalTitle = '新增分类';
+      },
+      editStructClassification () {
+        this.classificationModalVisible = true;
+        this.classificationModalTitle = '编辑分类';
+      },
+      deleteStructClassification () {
+        this.$confirm({
+          title: '提示',
+          content: '确定删除此分类（此操作不可逆）?',
+          onOk() {
+//            return new Promise((resolve, reject) => {
+//              setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+//            }).catch(() => console.log('Oops errors!'));
+          },
+          onCancel() {},
+        });
+      },
+      structTableCheck () {
+        this.$refs.structInfoModalForm.structModalVisible = true;
+        this.$refs.structInfoModalForm.title = '结构体信息';
+        this.$refs.structInfoModalForm.disableSubmit= true;
+      },
+      structTableEdit () {
+        this.$refs.structInfoModalForm.structModalVisible = true;
+        this.$refs.structInfoModalForm.title = '编辑';
+        this.$refs.structInfoModalForm.disableSubmit= false;
+      },
+      addStructInfo () {
+        this.$refs.structInfoModalForm.structModalVisible = true;
+        this.$refs.structInfoModalForm.title = '新增 ';
+        this.$refs.structInfoModalForm.disableSubmit= false;
+      }
+    },
+    created() {
+
+    }
+  }
+</script>
+<style scoped>
+  .struct-classification-list-title{
+    font-size: 18px;
+    font-weight: bold;
+    padding-right: 270px;
+  }
+  .struct-classification-list-add-icon{
+    font-size: 18px;
+    color: #2eabff;
+  }
+  .struct-classification-list-add-icon:hover{
+    cursor:pointer;
+  }
+  .struct-classification-list-item-icon{
+    font-size: 18px;
+    padding-right: 10px;
+  }
+  .struct-classification-list-item-name{
+    font-size: 18px;
+  }
+  .struct-classification-list-item-edit{
+    font-size: 18px;
+    float: right;
+    margin: 5px 20px 0 0;
+  }
+  .struct-classification-list-item-edit:hover{
+    cursor:pointer;
+  }
+  .struct-classification-list-item-delete{
+    font-size: 18px;
+    float: right;
+    margin: 5px 10px 0 0;
+  }
+  .struct-classification-list-item-delete:hover{
+    cursor:pointer;
+  }
+  .struct-classification-list-item{
+    padding: 8px 0 8px 0;
+  }
+  .struct-classification-list-item:hover{
+    background-color: #d9d9d9;
+  }
+</style>
